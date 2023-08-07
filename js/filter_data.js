4780eb0e8fa9b22c3d0e90e8bcab6ed9e272f9e9
Mario Hristov <81563209+MarioHristov@users.noreply.github.com>
2023-08-07 10:35:52 +0300

    Update filter_data.js

--- js/filter_data.js
+++ js/filter_data.js
@@ -47,6 +47,54 @@ async function fetchData() {
     const data =  await response.json();
     return data[category];
 }
+aconst category = new URLSearchParams(window.location.search).get('category');
+var finalData = {};
+var step = 0;
+async function applyFilters() {
+    var sections = document.querySelectorAll(".filter-section");
+    var filtersToApply = { "Color": [], "Brand": [], "PriceRange": [] };
+    try {
+        sections.forEach((section) => {
+            filtersToApply[section.id] = getSectionSelection(document.getElementById(`${section.id}`));
+        });
+
+        const filteredItems = await fetchDataFromJSONFile(filtersToApply);
+
+        return filteredItems;
+    } catch (error) {
+        console.error('Error applying filters:', error);
+    }
+}
+
+function getSectionSelection(sectionId) {
+    let result = [];
+    if (sectionId.id === "PriceRange") {
+        let prices = sectionId.querySelectorAll("input[type='number']");
+        prices.forEach((price) => {
+            result.push(price.value);
+        });
+    }
+    else {
+        let inputs = sectionId.querySelectorAll("input[type='checkbox']:checked");
+
+        inputs.forEach((input) => {
+            result.push(input.id);
+        });
+    }
+
+    return result;
+}
+async function fetchData() {
+    const response = await fetch('http://192.168.0.104:8080/json_data/products.json');
+
+    // Check if the response is successful
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+
+    const data =  await response.json();
+    return data[category];
+}
 async function fetchDataFromJSONFile(filters) {
     try {
         const data = await fetchData();
@@ -85,6 +133,136 @@ async function fetchDataFromJSONFile(filters) {
     }
 }
 
+function displayData(data) {
+    const container = document.querySelector(".products-grid");
+    container.innerHTML = "";
+    container.setAttribute("id", `${category}`);
+    if (data.length === 0) {
+        const noItemsMessage = document.createElement("p");
+        noItemsMessage.textContent = "No items match the selected criteria.";
+        container.appendChild(noItemsMessage);
+    }
+    else {
+        for (const itemId in data) {
+
+            const item = data[itemId];
+
+            const listItem = Object.assign(document.createElement("li"), { className: "grid-item", id: `${itemId}` });
+
+            const itemHeader = Object.assign(document.createElement("div"), { className: "item-header" });
+
+            itemHeader.innerHTML = `
+            <img src="${item.ImageSource}" alt="">
+            <i class="fa-solid fa-cart-shopping"></i>
+          `;
+            listItem.appendChild(itemHeader);
+
+            const itemBody = Object.assign(document.createElement("div"), { className: "item-body" });
+
+            itemBody.innerHTML = `
+            <div class="item-name">
+              <span>${item.Name}</span>
+            </div>
+            <div class="item-price-rating">
+              <span>${item.Price} $</span>
+              <div class="stars">
+                    <form action="">
+                        <input class="star star-5" id="star-5" type="radio" name="star" ${item.Rating === 5 ? "checked" : ""} disabled/>
+                        <label class="star star-5" for="star-5"></label>
+                        <input class="star star-4" id="star-4" type="radio" name="star" ${item.Rating === 4 ? "checked" : ""} disabled/>
+                        <label class="star star-4" for="star-4"></label>
+                        <input class="star star-3" id="star-3" type="radio" name="star" ${item.Rating === 3 ? "checked" : ""} disabled/>
+                        <label class="star star-3" for="star-3"></label>
+                        <input class="star star-2" id="star-2" type="radio" name="star" ${item.Rating === 2 ? "checked" : ""} disabled/>
+                        <label class="star star-2" for="star-2"></label>
+                        <input class="star star-1" id="star-1" type="radio" name="star" ${item.Rating === 1 ? "checked" : ""} disabled/>
+                        <label class="star star-1" for="star-1"></label>
+                    </form>
+                </div>
+            </div>
+            </div>
+            <div class="item-description">
+              <p>${item.Description}</p>
+            </div>
+          `
+
+            listItem.appendChild(itemBody);
+
+            container.appendChild(listItem);
+        }
+    }
+}
+async function setCategoryDetails(){
+    document.querySelector(".category-container").innerHTML = `
+    This our finest collection of <b>${category}</b>. We have over ${Object.keys(await fetchData()).length} items available to choose from. What are you waiting for, shop now!
+    `;
+}
+
+async function loadData() {
+    const maxItems = 20;
+    const element = document.getElementById("load-more");
+  
+    // Fetch filtered data
+    const filteredData = await applyFilters();
+    const keys = Object.keys(filteredData);
+  
+    // Display the elements on the page
+    let move = step * maxItems;
+
+
+    keys.slice(0, maxItems + move).forEach((key) => {
+      finalData[key] = filteredData[key];
+    });
+    step++;
+    // If all elements have been displayed, hide the button
+    if (keys.length <= maxItems + move) {
+      element.style.display = "none";
+    }
+  
+    displayData(finalData);
+  }
+  
+
+const filterHeaders = document.querySelectorAll(".filter-header");
+filterHeaders.forEach(header => {
+    header.addEventListener("click", function () {
+        // Get the closest .filter-body element from the clicked icon
+        const filterBody = header.closest(".filter-section").querySelector(".filter-body");
+        // Check if the filterBody is found
+        if (filterBody) {
+            // Add your action here using the filterBody element
+            const icon = header.querySelector(".fa-solid");
+
+            if (filterBody.style.display === "block") {
+                filterBody.style.display = "none";
+                icon.classList.remove("fa-chevron-up");
+                icon.classList.add("fa-chevron-down");
+            }
+            else {
+                filterBody.style.display = "block";
+                icon.classList.remove("fa-chevron-down");
+                icon.classList.add("fa-chevron-up");
+            }
+        } else {
+            console.log("No filter body found.");
+        }
+    });
+});
+
+const sortingButton = document.getElementById("sorting-button");
+
+sortingButton.addEventListener("click", function () {
+    const sortingOptions = document.querySelector(".sorting-options");
+    if (sortingOptions.style.display === "block") {
+        sortingOptions.style.display = "none";
+    }
+    else {
+        sortingOptions.style.display = "block";
+    }
+});
+
+loadData();
+setCategoryDetails();
 function displayData(data) {
     const container = document.querySelector(".products-grid");
     container.innerHTML = "";
